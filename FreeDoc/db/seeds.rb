42850cf60b7c59947fd59b16f303c872ca6532cd
typhain <48917508+typhain@users.noreply.github.com>
2019-08-02 03:45:15 +0200

    Update seeds.rb

--- FreeDoc/db/seeds.rb
+++ FreeDoc/db/seeds.rb
@@ -3,16 +3,9 @@ require 'faker'
   Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code)
 end
 
-# 50.times do
-#   Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-# end
-
-# 50.times do
-#   Appointment.create(date: Faker::Date.forward(days: 30), doctor_id: rand(1..50), patient_id: rand(1..50))
-# end()
 
 50.times do
-p  Appointment.create(date: Faker::Date.forward(days: 30))
+  Appointment.create(doctor: Doctor.all.sample, patient: Patient.all.sample, date: Faker::Date.forward(days: 30))
 end
 
 50.times do
